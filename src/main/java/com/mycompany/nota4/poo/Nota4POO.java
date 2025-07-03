2ea28a6103876f8bd3e49eb322fd8d74e95a4b59
BETTY <BETTY@Malu>
2025-07-03 01:20:05 -0500

    POO PROYECTO

---
+++ src/main/java/com/mycompany/nota4/poo/Nota4POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nota4.poo;
+
+/**
+ *
+ * @author BETTY
+ */
+public class Nota4POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
